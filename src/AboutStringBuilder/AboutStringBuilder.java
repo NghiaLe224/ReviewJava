787c618d866f8a6f9c19b913947f165a4239adc3
ltn.lenghia2@gmail.comgit config --global user.email ltn.lenghia2@gmail.com <138308068+ltnghia23@users.noreply.github.com>
2025-04-14 15:07:43 +0700

    update about StringBuilder

---
+++ src/AboutStringBuilder/AboutStringBuilder.java
@@ -0,0 +1,19 @@
+package AboutStringBuilder;
+
+
+public class StringBuilder {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printInformation(String string){
+        System.out.println("String: " + string);
+        System.out.println("length: " + string.length());
+    }
+
+    public static void printInformation(StringBuilder builder){
+        System.out.println("StringBuilder: " + builder);
+        System.out.println("length: " + builder.length());
+    }
+
+}
